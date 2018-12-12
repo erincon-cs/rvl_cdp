fa17e706aabd7fc1e944f23347fb6292236c04b6
Eric Rincon <erincon@cognitivescale.com>
2018-12-12 11:46:59 -0600

    Added base model

--- rvl_cdp/models/model.py
+++ rvl_cdp/models/model.py
@@ -1,12 +1,35 @@
 import torch
+import numpy as np
+
 from torchvision.models import densenet121
 
 import torch.nn as nn
 
 
-class DenseNet121(nn.Module):
+class BaseModel(nn.Module):
+    def __init__(self, name):
+        super(BaseModel, self).__init__()
+
+        self.name = name
+
+    def set_embeddings(self, vectors, freeze=True):
+        self.embedding.weight = nn.Parameter(vectors)
+        self.embedding.weight.requires_grad = not freeze
+
+    def predict(self, x):
+        preds = self.forward(x).cpu().numpy()
+
+        preds = np.argmax(preds, axis=1)
+
+        return preds
+
+    def save(self, path):
+        torch.save(self.state_dict(), path)
+
+
+class DenseNet121(BaseModel):
     def __init__(self, nb_classes=16, image_shape=(256, 256), pretrained=True):
-        super(DenseNet121, self).__init__()
+        super(DenseNet121, self).__init__("DenseNet121")
 
         self.nb_classes = nb_classes
         self.imgae_shape = image_shape
@@ -22,6 +45,3 @@ class DenseNet121(nn.Module):
         x = self.classifier(x[:, -1, :])
 
         return x
-
-    def save(self, path):
-        torch.save(self.state_dict(), path)
