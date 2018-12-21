4f3a7874d3a93962fe85f3624d90174490486802
Eric Rincon <erincon@cognitivescale.com>
2018-12-21 10:39:48 -0600

    Fixed dim passing arg error

--- rvl_cdp/models/model.py
+++ rvl_cdp/models/model.py
@@ -3,6 +3,7 @@ import numpy as np
 from torchvision.models import densenet121
 
 import torch.nn as nn
+import torch.distributions as tdist
 
 
 class BaseModel(nn.Module):
@@ -27,6 +28,24 @@ class BaseModel(nn.Module):
         torch.save(self.state_dict(), path)
 
 
+class Convolution2DReparameterization(nn.Module):
+    def __init__(self, *args, **kwargs):
+        super(Convolution2DReparameterization, self).__init__()
+
+        self.loc = nn.Conv2d(*args, **kwargs)
+        self.scale = nn.Conv2d(*args, **kwargs)
+
+        self.normal = tdist.Normal(torch.tensor([0.0]), torch.tensor([1.0]))
+
+    def forward(self, *input):
+        loc = self.loc(input)
+        scale = self.loc(input)
+
+        epsilon = self.normal.sample(loc.Size())
+
+        return (loc + scale) * epsilon
+
+
 class DenseNet121(BaseModel):
     def __init__(self, nb_classes=16, image_shape=(256, 256), pretrained=True):
         super(DenseNet121, self).__init__("DenseNet121")
@@ -38,7 +57,7 @@ class DenseNet121(BaseModel):
         self.conv_mapping = nn.Conv2d(1, 3, kernel_size=(1, 1))
         self.features = nn.Sequential(*list(densenet121(pretrained=pretrained).children())[:-1])
         self.classifier = nn.Linear(8, self.nb_classes)
-        self.softmax = nn.Softmax()
+        self.softmax = nn.Softmax(dim=1)
 
     def forward(self, x):
         x = self.conv_mapping(x)
@@ -49,7 +68,7 @@ class DenseNet121(BaseModel):
 
     def predict(self, x):
         preds = self.forward(x).cpu()
-        preds = self.softmax(preds, dim=1).numpy()
+        preds = self.softmax(preds).numpy()
 
         preds = np.argmax(preds, axis=1)
 
