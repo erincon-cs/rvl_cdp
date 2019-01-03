632fd4c234d5296e989d6baa0ad18f71789e1410
Eric Rincon <erincon@cognitivescale.com>
2019-01-03 16:38:29 -0600

    cleaned up script

--- train.py
+++ train.py
@@ -3,15 +3,8 @@ import os
 
 from rvl_cdp.training import Trainer
 from rvl_cdp.models.factory import get_model
-from rvl_cdp.data.factory import get_dataset
-from torchvision.transforms import Compose
-from rvl_cdp.data.dataset import Resize, ToTensor, \
-    Normalization, RandomImageCrop
 
-
-def check_exist_path(path):
-    if not os.path.exists(path):
-        raise ValueError("Path {} does not exist".format(path))
+from rvl_cdp.data.factory import load_datasets
 
 
 def summary_path_path(arg):
@@ -23,16 +16,28 @@ def summary_path_path(arg):
         return arg
 
 
+_dataset_kwargs = {
+    "rvlcdip": {"two_dim_map": True},
+    "cifar10": {}
+}
+
+
+def get_model_kwargs(dataset_name):
+    dataset_name = dataset_name.lower()
+
+    if dataset_name not in _dataset_kwargs:
+        raise ValueError("Dataset {} not defined!".format(dataset_name))
+
+    return _dataset_kwargs[dataset_name]
+
+
 def main():
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument("--model", default="dn121")
     arg_parser.add_argument("--dataset", default='rvlcdip', type=str,
                             help="Name of the dataset.\nThe default dataset is rvlcdip"
                                  "which is just the raw RVL-CDIP data")
-    arg_parser.add_argument("--images", default='data/images',
-                            type=str, help="Directory path to the images")
-    arg_parser.add_argument("--labels", default="data/labels",
-                            help="Directory path to the labels")
+    arg_parser.add_argument("--data", type=str, help="Directory where the data is stored")
     arg_parser.add_argument("--num-workers", default=3, type=int,
                             help="The number of workers to use in the DataLoader")
     arg_parser.add_argument("--lr", default=0.0001, type=float, help="Learning rate")
@@ -40,25 +45,15 @@ def main():
     arg_parser.add_argument("--summary-path", default="none", type=summary_path_path)
     args = arg_parser.parse_args()
 
-    Dataset = get_dataset("rvlcdip")
-
-    check_exist_path(args.labels)
-    check_exist_path(args.images)
-
-    train_labels = os.path.join(args.labels, "train.txt")
-    valid_labels = os.path.join(args.labels, "val.txt")
-    test_labels = os.path.join(args.labels, "test.txt")
-
-    train_dataset = Dataset(images_path=args.images, labels_path=train_labels)
-    valid_dataset = Dataset(images_path=args.images, labels_path=valid_labels)
-    test_dataset = Dataset(images_path=args.images, labels_path=test_labels)
+    datasets = load_datasets(args.data, args.dataset)
 
     Model = get_model(args.model)
-    model = Model()
+    model_kwwargs = get_model_kwargs(args.dataset)
+    model = Model(**model_kwwargs)
 
-    trainer = Trainer(model, train_dataset, valid_dataset, test_dataset, summary_path=args.summary_path)
+    trainer = Trainer(model, summary_path=args.summary_path, **datasets)
     trainer.fit(learning_rate=args.lr, minibatch_size=args.mb_size)
-    trainer.evaluate(test_dataset)
+    trainer.evaluate(datasets["test_dataset"])
 
 
 if __name__ == '__main__':
