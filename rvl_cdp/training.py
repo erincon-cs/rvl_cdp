c34de97b2b1bacd5ccb421c52057c22aa45c1742
Eric Rincon <erincon@cognitivescale.com>
2019-01-07 14:36:55 -0600

    Made iteratuion

--- rvl_cdp/training.py
+++ rvl_cdp/training.py
@@ -102,7 +102,8 @@ class Trainer:
                 end = time.time()
                 running_time += (end - start)
 
-                avg_loss = running_loss / ((minibatch_i + 1) * minibatch_size)
+                avg_loss = running_loss / (minibatch_size * iteration)
+
                 avg_mb_time = running_time / (minibatch_i + 1)
                 sys.stdout.write("\r" + "running loss: {0:.5f}".format(avg_loss) + \
                                  " - average time minibatch: {0:.2f}s".format(avg_mb_time))
@@ -112,59 +113,61 @@ class Trainer:
 
                 if keep_preds:
                     preds = self.model.predict(image)
-                    y.append(preds)
+                y.append(preds)
 
-            if keep_preds:
-                y = np.hstack(y)
+                if keep_preds:
+                    y = np.hstack(y)
                 y_true = np.hstack(y_true)
 
-            if self.valid_dataset and network_optimizer is not None:
-                accuracy, valid_loss = self.evaluate(self.valid_dataset)
+                if self.valid_dataset and network_optimizer is not None:
+                    accuracy, valid_loss = self.evaluate(self.valid_dataset)
 
                 self.writer.add_scalar("valid_loss", valid_loss, i)
                 self.writer.add_scalar("valid_accuracy", accuracy, i)
 
                 print("\nEpoch {} loss: {}".format(i, np.mean(total_loss)))
 
-        return total_loss, y, y_true
+    return total_loss, y, y_true
 
-    def fit(self, nb_epochs=10, learning_rate=0.0001,
-            num_workers=3, minibatch_size=64, verbose=True):
-        self.trained = True
 
-        network_optimizer = optim.Adam(self.model.parameters(), lr=learning_rate)
-        criterion = nn.CrossEntropyLoss()
-        data_loader = DataLoader(self.training_dataset, batch_size=minibatch_size,
-                                 shuffle=True, num_workers=num_workers)
-        training_loss, _, _ = self._data_loop(data_loader, nb_epochs, criterion, minibatch_size, network_optimizer,
-                                              keep_preds=False)
+def fit(self, nb_epochs=10, learning_rate=0.0001,
+        num_workers=3, minibatch_size=64, verbose=True):
+    self.trained = True
 
-    def evaluate(self, dataset, data_loader=None, minibatch_size=64):
-        if not self.trained:
-            raise UserWarning("Model is not trained yet!")
+    network_optimizer = optim.Adam(self.model.parameters(), lr=learning_rate)
+    criterion = nn.CrossEntropyLoss()
+    data_loader = DataLoader(self.training_dataset, batch_size=minibatch_size,
+                             shuffle=True, num_workers=num_workers)
+    training_loss, _, _ = self._data_loop(data_loader, nb_epochs, criterion, minibatch_size, network_optimizer,
+                                          keep_preds=False)
 
-        if data_loader is None:
-            data_loader = DataLoader(dataset, batch_size=minibatch_size,
-                                     shuffle=False, num_workers=3)
 
-        print("Evaluating on {} examples".format(len(data_loader)))
+def evaluate(self, dataset, data_loader=None, minibatch_size=64):
+    if not self.trained:
+        raise UserWarning("Model is not trained yet!")
 
-        criterion = nn.CrossEntropyLoss()
+    if data_loader is None:
+        data_loader = DataLoader(dataset, batch_size=minibatch_size,
+                                 shuffle=False, num_workers=3)
 
-        if torch.cuda.is_available():
-            self.model.cuda()
+    print("Evaluating on {} examples".format(len(data_loader)))
 
-        running_time, avg_loss = 0, 0
+    criterion = nn.CrossEntropyLoss()
 
-        with torch.no_grad():
-            training_loss, y, y_true = self._data_loop(data_loader, 1, criterion, minibatch_size,
-                                                       network_optimizer=None, keep_preds=True)
-            accuracy = accuracy_score(y, y_true)
+    if torch.cuda.is_available():
+        self.model.cuda()
 
-            print("\nAccuracy: {0:.2f}".format(accuracy))
+    running_time, avg_loss = 0, 0
 
-            if accuracy > self.max_score:
-                self.max_score = accuracy
-                self.model.save("best_model.model")
+    with torch.no_grad():
+        training_loss, y, y_true = self._data_loop(data_loader, 1, criterion, minibatch_size,
+                                                   network_optimizer=None, keep_preds=True)
+        accuracy = accuracy_score(y, y_true)
 
-            return accuracy, avg_loss
+        print("\nAccuracy: {0:.2f}".format(accuracy))
+
+        if accuracy > self.max_score:
+            self.max_score = accuracy
+            self.model.save("best_model.model")
+
+        return accuracy, avg_loss
