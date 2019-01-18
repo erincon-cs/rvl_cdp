70d1b6b86b05dbd016bf260634d729b088528bfe
Eric Rincon <erincon@cognitivescale.com>
2019-01-18 12:00:17 -0600

    Modifed loss function

--- rvl_cdp/models/loss.py
+++ rvl_cdp/models/loss.py
@@ -8,5 +8,5 @@ class KLNormal(nn.Module):
         super(KLNormal, self).__init__()
 
     def forward(self, inputs_mean, inputs_std, target_mean, target_std):
-        return torch.log(target_std) - torch.log(inputs_std) + target_std ** 2 + (((inputs_mean - target_mean) ** 2) \
+        return torch.log(inputs_std) - torch.log(target_std) + target_std ** 2 + (((inputs_mean - target_mean) ** 2) \
                                                                                   / (2 * target_std ** 2)) - 1 / 2
