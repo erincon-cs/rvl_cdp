4489018cb56bc57f38f884fe6cf9f8c8e7b59ad7
Eric Rincon <erincon@cognitivescale.com>
2019-02-02 13:16:18 -0600

    Forgot to push train.py

--- train.py
+++ train.py
@@ -1,44 +1,10 @@
 import argparse
-import os
 
+from rvl_cdp.arg_utils import none_arg_path, boolean, get_model_kwargs
 from rvl_cdp.training import Trainer
 from rvl_cdp.models.factory import get_model
 
 from rvl_cdp.data.factory import load_datasets
-
-
-def none_arg_path(arg):
-    arg = arg.lower()
-
-    if arg == "none":
-        return None
-    else:
-        return arg
-
-
-def boolean(arg):
-    if arg in ("y", "yes", "1", "true", "t"):
-        return True
-    else:
-        return False
-
-
-_dataset_kwargs = {
-    "rvlcdip": {"two_dim_map": True},
-    "cifar10": {"nb_classes": 10},
-    "food101": {"nb_classes": 101}
-}
-
-
-def get_model_kwargs(dataset_name):
-    dataset_name = dataset_name.lower()
-
-    if dataset_name not in _dataset_kwargs:
-        raise ValueError("Dataset {} not defined!".format(dataset_name))
-
-    model_kwargs = _dataset_kwargs[dataset_name]
-
-    return model_kwargs
 
 
 def main():
